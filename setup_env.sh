1830258d1a4289c9f6790aeb4cef69d72335568b
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-09 08:21:34 +0200

    auto commit

---
+++ setup_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+showLoading() {
+	loadingText=$1
+
+	echo -ne "$loadingText\r "
+	while kill -0 $! &>/dev/null; do
+		echo -ne "\r\033[K"
+		echo -ne "${WHITE}$loadingText .\r"
+		sleep 0.5
+		echo -ne "${WHITE}$loadingText ..\r"
+		sleep 0.5
+		echo -ne "${WHITE}$loadingText ...\r"
+		sleep 0.5
+		echo -ne "\r\033[K"
+		echo -ne "${WHITE}$loadingText \r"
+		sleep 0.5
+	done
+	echo "${WHITE}"
+}
+
+setup() {
+	(cat vimrc >> ~/.vimrc & showLoading "vimrc settings")
+	(sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)" & showLoading "Installing Brew")
+	(cat gitall >> ~/.zshrc & showLoading "adding gitall")
+	(sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" & showLoading "Install oh-my-zsh")
+	(brew install --HEAD valgrind & showLoading "Installing valgrind")
+	(osascript ./dark_mode.scpt & showLoading "Changing theme to dark mode")
+	echo "Settings setup completed"
+}
+
+setup
