0a6d2d0790205f98bb56c77268b77473fc825a6c
Marco FERNANDES <mafernan@robotic-lab-3.wethinkcode.co.za>
2018-11-01 16:29:55 +0200

    auto files

--- auto/run.sh
+++ auto/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-echo "Open settings"
-osascript open_mouse.scpt
-sleep 2s
+#echo "Open settings"
+#osascript open_mouse.scpt
+#sleep 2s
+#
+#echo "Running mouse clicks setup"
+#./mouse -w 1000 c:260,87 c:265,147 c:90,40 c:210,220 c:280,168 c:522,168 c:95,253 c:90,40 c:300,102 c:44,158 c:20,40 c:20,40
 
-echo "Running mouse clicks setup"
-./mouse -w 1000 c:260,87 c:265,147 c:90,40 c:210,220 c:280,168 c:522,168 c:95,253 c:90,40 c:300,102 c:44,158 c:20,40 c:20,40
+echo "Open new terminal"
+osascript terminal.scpt
+sleep 5s
+
+echo "Running Brew install"
+./mouse -w 1000 t:"sleep 10s" kp:enter t:"$PWD/test.sh" kp:enter
+
